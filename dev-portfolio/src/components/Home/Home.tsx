27bf153630b9da77bed84af580014746eac7c13e
Mohammed-Yassine BOUMEHDI <myboumehdi.2002@gmail.com>
2023-11-15 00:33:41 +0100

    Starting home page

--- dev-portfolio/src/components/Home/Home.tsx
+++ dev-portfolio/src/components/Home/Home.tsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import './Home.css'
 
 function Home() {
   return (
-    <div>Home</div>
+    <>
+      <div className="general">
+        {/* <img src={} alt="me"> */}
+          <span>
+              <h3>Hi, my name is</h3>
+              <h1>Mohammed<span>-</span>Yassine BOUMEHDI.</h1>
+              <p>Innovate, Code, Deliver.</p>
+          </span>
+      </div>
+      <div className="social">
+        <a href="/resources/mycv.pdf" target="_blank"><button>Resume</button></a>
+          <ul>    
+            <li><a href="https://github.com/MYassineBoum" target="_blank"><i className="bi bi-github"></i></a></li>
+            <li><a href="https://www.linkedin.com/in/myassineboumehdi/" target="_blank"><i className="bi bi-linkedin"></i></a></li>
+            <li><a href="https://myboum.me/" target="_blank"><i className="bi bi-globe"></i></a></li>
+          </ul>
+      </div>
+    </>
   )
 }
 
