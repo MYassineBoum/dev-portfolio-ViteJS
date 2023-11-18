a5d67464eb05a8074249bc4cefd0da6209463af2
Mohammed-Yassine BOUMEHDI <myboumehdi.2002@gmail.com>
2023-11-18 02:22:00 +0100

    Small fix home

--- dev-portfolio/src/components/Home/Home.tsx
+++ dev-portfolio/src/components/Home/Home.tsx
@@ -8,7 +8,7 @@ function Home() {
     <>
       <div className="first-zone">
         <h1>Hi<span>,</span> I'm Mohammed<span>-</span>Yassine<br />BOUMEHDI<span>.</span></h1>
-        <h2>Software Engineer<span>,</span><br />Competitive Programme<span>,</span><br />DevOps <span>&</span> Cloud Enthusiast<span>.</span></h2>
+        <h2>Software Engineer<span>,</span><br />Competitive Programmer<span>,</span><br />DevOps <span>&</span> Cloud Enthusiast<span>.</span></h2>
         <button>Resume <span><Icon.Download /></span></button>
       </div>
       <div className="second-zone">
