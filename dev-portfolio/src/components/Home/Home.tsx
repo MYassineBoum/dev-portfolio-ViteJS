46e152724c3d371e3630ec1224926dd775350fda
Mohammed-Yassine BOUMEHDI <myboumehdi.2002@gmail.com>
2023-11-22 20:52:52 +0100

    fixed active routes + added tsparticles

--- dev-portfolio/src/components/Home/Home.tsx
+++ dev-portfolio/src/components/Home/Home.tsx
@@ -2,20 +2,24 @@ import React from 'react'
 import './Home.css'
 import { Download } from 'react-bootstrap-icons';
 import * as Icon from 'react-bootstrap-icons';
+import ParticlesBG from '../ParticlesBG/ParticlesBG';
 
 function Home() {
   return (
     <>
-      <div className="first-zone">
-        <h1>Hi<span>,</span> I'm Mohammed<span>-</span>Yassine<br />BOUMEHDI<span>.</span></h1>
-        <h2>Software Engineer<span>,</span><br />Competitive Programmer<span>,</span><br />DevOps <span>&</span> Cloud Enthusiast<span>.</span></h2>
-        <button>Resume <span><Icon.Download /></span></button>
-      </div>
-      <div className="second-zone">
-        <div className="image-container">
-          <img src="Capture.PNG" alt="Personal photo." />
-          <span className="highlight-line-h"></span>
-          <span className="highlight-line-v"></span>
+      <ParticlesBG />
+      <div className='home-zone'>
+        <div className="first-zone">
+          <h1>Hi<span>,</span> I'm Mohammed<span>-</span>Yassine<br />BOUMEHDI<span>.</span></h1>
+          <h2>Software Engineer<span>,</span><br />Competitive Programmer<span>,</span><br />DevOps <span>&</span> Cloud Enthusiast<span>.</span></h2>
+          <button>Resume <span><Icon.Download /></span></button>
+        </div>
+        <div className="second-zone">
+          <div className="image-container">
+            <img src="Capture.PNG" alt="Personal photo." />
+            <span className="highlight-line-h"></span>
+            <span className="highlight-line-v"></span>
+          </div>
         </div>
       </div>
     </>
--- dev-portfolio/src/components/Layout/Layout.tsx
+++ dev-portfolio/src/components/Layout/Layout.tsx
@@ -1,16 +1,19 @@
-import { Outlet, Link } from 'react-router-dom'
+import { Outlet, Link, useLocation } from 'react-router-dom'
 import './Layout.css'
 
 function Layout() {
+    const location = useLocation();
+    const currentPath = location.pathname;
+
     return (
         <>
             <nav>
                 <ul>
-                    <li><a><Link to="/">Home</Link></a></li>
-                    <li><a><Link to="/about">About</Link></a></li>
-                    <li><a><Link to="/projects">Projects</Link></a></li>
-                    <li><a><Link to="/experiences">Experiences</Link></a></li>
-                    <li><a><Link to="/contact">Contact</Link></a></li>
+                    <li><a><Link className={currentPath == "/" ? "active" : ""} to="/">Home</Link></a></li>
+                    <li><a><Link className={currentPath == "/about" ? "active" : ""} to="/about">About</Link></a></li>
+                    <li><a><Link className={currentPath == "/projects" ? "active" : ""} to="/projects">Projects</Link></a></li>
+                    <li><a><Link className={currentPath == "/experiences" ? "active" : ""} to="/experiences">Experiences</Link></a></li>
+                    <li><a><Link className={currentPath == "/contact" ? "active" : ""} to="/contact">Contact</Link></a></li>
                 </ul>
             </nav>
             <Outlet />
