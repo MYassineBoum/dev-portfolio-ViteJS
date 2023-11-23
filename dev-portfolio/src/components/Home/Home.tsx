aec6eea2ca8bb5c9e0ec30a3546ef4f3f8f54d1f
Mohammed-Yassine BOUMEHDI <myboumehdi.2002@gmail.com>
2023-11-23 12:35:09 +0100

    adding footer

--- dev-portfolio/src/components/Home/Home.tsx
+++ dev-portfolio/src/components/Home/Home.tsx
@@ -8,18 +8,16 @@ function Home() {
   return (
     <>
       <ParticlesBG />
-      <div className='home-zone'>
-        <div className="first-zone">
-          <h1>Hi<span>,</span> I'm Mohammed<span>-</span>Yassine<br />BOUMEHDI<span>.</span></h1>
-          <h2>Software Engineer<span>,</span><br />Competitive Programmer<span>,</span><br />DevOps <span>&</span> Cloud Enthusiast<span>.</span></h2>
-          <button>Resume <span><Icon.Download /></span></button>
-        </div>
-        <div className="second-zone">
-          <div className="image-container">
-            <img src="Capture.PNG" alt="Personal photo." />
-            <span className="highlight-line-h"></span>
-            <span className="highlight-line-v"></span>
-          </div>
+      <div className="first-zone">
+        <h1>Hi<span>,</span> I'm Mohammed<span>-</span>Yassine<br />BOUMEHDI<span>.</span></h1>
+        <h2>Software Engineer<span>,</span><br />Competitive Programmer<span>,</span><br />DevOps <span>&</span> Cloud Enthusiast<span>.</span></h2>
+        <button>Resume <span><Icon.Download /></span></button>
+      </div>
+      <div className="second-zone">
+        <div className="image-container">
+          <img src="Capture.PNG" alt="Personal photo." />
+          <span className="highlight-line-h"></span>
+          <span className="highlight-line-v"></span>
         </div>
       </div>
     </>
---
+++ dev-portfolio/src/components/footer/footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function footer() {
+  return (
+    <>
+        <ul>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+        </ul>
+    </>
+  )
+}
